25f9ce518e135691e0e7d1d23caf6cc2e86ee508
hemantkumar2323 <freelanceb23@gmail.com>
2025-04-23 17:07:51 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ScalingBot from './ScalingBot.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ScalingBot />
+  </React.StrictMode>
+)
